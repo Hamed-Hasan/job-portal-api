1e75c974d75e4688139c6037d1fa5b585e0b289a
John Doe <hamedhasan422421@gmail.com>
2022-10-13 21:08:47 +0300

    adding mongoose candidate schema design

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = require('./app');
 //     console.log(`Database Connect Successfully`)
 // });
 mongoose.connect(`mongodb://${process.env.USER}:${process.env.PASSWORD}@ac-1njrd1n-shard-00-00.rapajqb.mongodb.net:27017,ac-1njrd1n-shard-00-01.rapajqb.mongodb.net:27017,ac-1njrd1n-shard-00-02.rapajqb.mongodb.net:27017/?ssl=true&replicaSet=atlas-116a99-shard-0&authSource=admin&retryWrites=true&w=majority`).then(() => {
-    console.log(`Database Connect Successfully`)
+    console.log(`Database Connect Successfully` .gray.bold)
 });
 
 // server port
