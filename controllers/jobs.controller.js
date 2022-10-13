ab9efccbbad28853570c20e12fe4c4cc51f79158
John Doe <hamedhasan422421@gmail.com>
2022-10-13 22:46:58 +0300

    create job controller

---
+++ controllers/jobs.controller.js
@@ -0,0 +1,19 @@
+const { createJobsService } = require("../services/jobs.service")
+
+exports.createJob = async (req, res, next) => {
+
+    try {
+        const jobs = await createJobsService(req.body)
+        res.status(200).json({
+            status: 'success',
+            message: 'Job created successfully',
+            data: jobs
+        })
+    } catch (error) {
+        res.status(400).json({
+            status:"fail",
+            message: "Data is not inserted",
+            error : error.message
+          })
+    }
+}
