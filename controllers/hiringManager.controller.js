3521fa431b5e334311a5c42147fa565fd34774af
John Doe <hamedhasan422421@gmail.com>
2022-10-14 00:09:32 +0300

    getting hiring manager controller

--- controllers/hiringManager.controller.js
+++ controllers/hiringManager.controller.js
@@ -18,3 +18,20 @@ exports.createHiringManager = async (req, res, next) => {
           })
     }
 }
+exports.getHiringManager = async (req, res, next) => {
+
+    try {
+        const jobs = await ()
+        res.status(200).json({
+            status: 'success',
+            message: 'manager created successfully',
+            data: jobs
+        })
+    } catch (error) {
+        res.status(400).json({
+            status:"fail",
+            message: "manager is not inserted",
+            error : error.message
+          })
+    }
+}
