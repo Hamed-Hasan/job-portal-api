fab9839567f9245b43a928239791b4556a1254ce
John Doe <hamedhasan422421@gmail.com>
2022-10-13 23:19:24 +0300

    get job service

--- services/jobs.service.js
+++ services/jobs.service.js
@@ -4,3 +4,7 @@ exports.createJobsService = async (data) => {
     const jobs = await Jobs.create(data);
     return jobs
 }
+exports.getJobsService = async () => {
+    const jobs = await Jobs.find({});
+    return jobs
+}
