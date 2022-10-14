8a0f3453c3893e0269a04ee7a1481dca3fbf9d7c
John Doe <hamedhasan422421@gmail.com>
2022-10-14 09:51:44 +0300

    adding admin routes

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const adminController = require("../controllers/admin.controller");
 
 router.get('/candidate', adminController.getCandidate)
-
+router.get('/admin/manager/', adminController.getManager)
+router.get('/candidate/:id', adminController.getCandidateId)
 
 module.exports = router;
